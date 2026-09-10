e68a53e730d7de42de573d7634cb34f083eefb90
sisi-design <lawin2111@gmail.com>
2026-09-11 08:55:52 +0900

    Add lyrics updates and separate album music section

--- lyrics-videos.js
+++ lyrics-videos.js
@@ -13,7 +13,8 @@
     "zui-langman":{id:"DWtZvfMpuZc",kind:"라이브 영상",note:"칠석 특별기획 무대"},
     "youxi-shijian":{id:"XpEeYkjyCLc",kind:"콘서트 대표영상",note:"2024 《此刻 X 以光》 무대"},
     "wayfarer":{id:"xvRoujeGUgw",kind:"콘서트 대표영상",note:"난징 공연 직캠"},
-    "yemeng-chunyou":{id:"Z2tpUEvPdaE",kind:"콘서트 대표영상",note:"2026.09.05 청두 공연"}
+    "yemeng-chunyou":{id:"Z2tpUEvPdaE",kind:"콘서트 대표영상",note:"2026.09.05 청두 공연"},
+    "huixide-tong":{id:"LgtkSSE93P8",kind:"첫 솔로 콘서트 영상",note:"2024 《此刻 X 以光》 무대"}
   };
 
   function makeVideo(song,video){
